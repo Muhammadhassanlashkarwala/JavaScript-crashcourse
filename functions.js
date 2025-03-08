5b5d2d6f93af4935562e537c87ad0c481fef3516
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-03-09 04:55:17 +0500

    functions file added

---
+++ functions.js
@@ -0,0 +1,11 @@
+
+
+// Functions In JavaScript 🔥
+
+// In javscript, a function is a block of reusable code that performs a specific task or set of tasks. Functions re used to  organize code into modular and managebale pieces, promote code reuse, and make program more readable.
+
+// Now simple Example.
+
+// 3 students at a time wants to find the sum of two numbers.
+
+// 1st Student:
