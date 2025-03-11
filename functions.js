dd7ff20f9126d957839ad9d192e1829d541ee04f
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-03-11 08:21:46 +0500

    function parameter, invocation, declaration, arguments donegit add .

--- functions.js
+++ functions.js
@@ -9,3 +9,91 @@
 // 3 students at a time wants to find the sum of two numbers.
 
 // 1st Student:
+
+// var a = 10, b = 20;
+// var sum = a + b;
+// console.log(sum);
+
+// // 2nd Student:
+
+// var a = 25, b = 40;
+// var sum = a + b;
+// console.log(sum);
+
+// // 3rd Student:
+
+// var a = 20, b = 75;
+// var sum = a + b;
+// console.log(sum);
+
+
+// function sum(a, b){
+//    return a + b;
+// }
+// console.log(sum(5, 15));
+// console.log(sum(25, 15));
+// console.log(sum(60, 180));
+
+
+
+
+// Function Declaration:
+// function sum(){
+//    console.log("Hey everyone!!");
+// };
+
+
+// Function Invocation:
+// Means how to call a function. So only you call a function name and perenthesis.
+// sum();
+
+
+
+// Now practice time:
+// Write a function to find the sum of two numbers.
+
+// todo tips "first you declare the function & then you call it". In Javascript, it's a good practice to declare (define) your functions before you call them. This ensures that the function is available for use when you try to call it.
+
+
+
+
+//  Function Parameter:
+// function sum(a, b, d){
+//   console.log( a + b + d);
+// }
+
+
+// Function Arguments:
+// sum(5, 10, 20);
+// sum(20, 40, 180);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
