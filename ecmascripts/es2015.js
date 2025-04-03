77545c95c04ba1379a1c9550da12db67bfa9511b
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-04-04 04:42:34 +0500

    Let, const, var difference

--- ecmascripts/es2015.js
+++ ecmascripts/es2015.js
@@ -6,14 +6,79 @@
 // Let keyword:
 // The Let keyword is used to declare variables with block scope. Variables declared with let are mutable, meaning their values can be reassigned.
 
+// var myName = "Hassan";
+// myName = "Owais"
+// console.log(myName); 
+
+// let myName = "Hassan";
+// myName = "Ahmed";
+// console.log(myName);
+
+// const myFavName = "Hassan Raza";
+// myFavName = "Shahzaib"
+// console.log(myFavName);
+
+// First Example with the var keyword:
+
+// var myName = "Hassan";
+
+// if(true){
+//     var myName = "Raza";
+//     console.log(myName); 
+// }
+// myName = "Owais";
+// console.log(myName);
+
+
+// Second Example with the help of let keyword:
+
+// let myName = "Hassan";
+
+// if(true){
+//     let myName = "Raza";
+//     console.log(myName); 
+// }
+// myName = "Owais";
+// console.log(myName);
+
+
+// Third Example with the help of const keyword !!
+
+// const myName = "Hassan";   // true
+
+// if(true){
+//     const myName = "Raza";  // false
+//     console.log(myName); 
+// }
+// myName = "Owais";
+// console.log(myName);
+
+// var
+
+// if(true){
+//     var myName = "Raza";  
+//     console.log(myName); 
+// }
+//  myName = "Owais";
+// console.log(myName);
+
+
+// let :
+
+// if(true){
+//     const myName = "Let";  
+//     console.log(myName); 
+// }
+// console.log(myName);
+
+
 // Const Keyword:
 // The const keyword is used to declare variables with block scope but once a value is assigned to a const variable. Const variables are immutable and it cannot be reassigned.
 
-// guyzz appko yaad hooga jab humne switch statement dekha tha usme area of circle ki example dekhi thi right and area of circle ki value kiya hooti h hamare pass ??
-// This is: pi r * r
+// This is : pi r * r
 
-// and appko pata h pi ki value hamare pass hooti h pi = 3.142
-// Tww ye ju pi ki value ki h hamare pass it is constant value is value ko koi change nh kr sakta app ye nh bol sakte ke pi equal to 4.562 no kyu ke it is constant value means ye value change nh ho sakti isii tarah hamare pass ye const ka keyword hoota h ke jab bh is const ke keyword se humne variable assigned kr diya tww iski value change nh ho sakti ab kyu ke ye kiya ban chuki constant value i hope kaafi hadh tak appko samj agaya hoo lekin ab hum practical bh kr ke dekh leete hen taake appko aur ache se samj ajai OK. 
+// pi = 3.142
+ 
 
 
 // TEMPLATE String (``)📑
