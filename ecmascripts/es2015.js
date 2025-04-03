90e50f760a391889bdefdc9f2332bf6ddcc2cd0e
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-04-04 03:39:57 +0500

    ECMA Script

---
+++ ecmascripts/es2015.js
@@ -0,0 +1,21 @@
+// Modern JavaScript ECMA-Script 2015 ✨
+
+// LET AND CONST 📌
+
+
+// Let keyword:
+// The Let keyword is used to declare variables with block scope. Variables declared with let are mutable, meaning their values can be reassigned.
+
+// Const Keyword:
+// The const keyword is used to declare variables with block scope but once a value is assigned to a const variable. Const variables are immutable and it cannot be reassigned.
+
+// guyzz appko yaad hooga jab humne switch statement dekha tha usme area of circle ki example dekhi thi right and area of circle ki value kiya hooti h hamare pass ??
+// This is: pi r * r
+
+// and appko pata h pi ki value hamare pass hooti h pi = 3.142
+// Tww ye ju pi ki value ki h hamare pass it is constant value is value ko koi change nh kr sakta app ye nh bol sakte ke pi equal to 4.562 no kyu ke it is constant value means ye value change nh ho sakti isii tarah hamare pass ye const ka keyword hoota h ke jab bh is const ke keyword se humne variable assigned kr diya tww iski value change nh ho sakti ab kyu ke ye kiya ban chuki constant value i hope kaafi hadh tak appko samj agaya hoo lekin ab hum practical bh kr ke dekh leete hen taake appko aur ache se samj ajai OK. 
+
+
+// TEMPLATE String (``)📑
+
+// In ECMAScript 6 (ES6), template strings, also known as template literals, provide a convenient and flexible way to create string in JavaScript. Template strings are enclosed in backticks (``) rather then single or double quotes
