9b71443a923e092a47854cebfc423096d0aebb73
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-05-12 02:51:01 +0500

    String session start !

---
+++ strings.js
@@ -0,0 +1,50 @@
+// =============================
+//  STRINGS IN JAVASCRIPT ("") ✨📌
+// =============================
+
+//* Strings in JavaScript are a fundamental data type that represents a sequence of characters.
+
+// Note:
+// Strings created with single or double quotes (""), ('') both are same and the works also same.
+// There is no difference between the two! 
+
+//* String Properties
+// Length: Property that returns the length of the string (number of characters).
+
+// const str = "Hello, World   !";
+// console.log(str.length);
+// Length keyword consider all words & space also!
+
+// ===================
+// ESCAPE CHARACTER
+// ===================
+
+//* Escape Character: In JavaScript, the backslash \ is used as an escape character. It allows you to include special characters in a string.
+// code         result         description
+
+// \'            '               Single quote
+// \"            "               Double quote
+// \\            '               Backslash    
+
+// let text = "My name is Hassan Raza & I am a Full Stack Developer!"; 
+// let text = "My name is 'Hassan Raza' & I am a \"Full Stack \" Developer!"; 
+// let text = 'My name is Hassan Raza \\ & I am a \'Full Stack\' Developer!'; 
+// console.log(text);
+
+// ========================
+// STRING SEARCH METHODS
+// ========================
+
+//* String search methods:
+//a: The indexOf(): The indexOf() method returns the index (position) of the first occurence of a string in a string, or it returns -1 if the string is not found:
+// syntax
+// indexOf(searchString)
+// indexOf(searchString, position)
+
+// let text = "Hassan Raza";  
+
+//b: The lastIndexOf(): The lastIndexOf() method returns the index of the last occurence of a specified text in a string:
+// syntax
+// lastIndexOf(searchString)
+// lastIndexOf(searchString, positionlastIndex
+
