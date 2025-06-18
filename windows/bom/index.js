73b65629a6228927655afb62bfd047c05dad9869
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-06-18 18:00:53 +0500

    Add window object

---
+++ windows/bom/index.js
@@ -0,0 +1,19 @@
+// ==========================
+//  BOM Properties:
+// ==========================
+
+// Window Properties: 🔄
+
+//? window.console:
+// Represents the browser's debugging console.
+// Allows logging messages using methods like log(), warn(), & error().
+
+//? window.innerWidth / window.innerHeight:
+// Provides the width and height of the browser's content area     (excluding toolbars and scrollbars).
+
+//? window.scrollX / window.scrollY:
+// Represents the number of pixels that the document is currently scrolled horizontally and vertically.
+
+//? window.outerWidth / window.outerHeight:
+// Provides the width and height of the whole browser window(including toolbars and borders).
+
