d67258110d4dbf46bcc577faa43d49049baa166b
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-07-16 22:24:10 +0500

    DOM start

---
+++ windows/dom/index.js
@@ -0,0 +1,5 @@
+// ==========================
+//  DOM In JavaScript:
+// ==========================
+
+// When a web browser loads an HTML document, it parses the HTML source codes and creates a tree like structure known as the Document Object Model (DOM). This DOM tree represents the structure of the HTML document, with each HTML element being a node in the tree.
