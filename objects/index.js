6a892d9ce0e7c515f7ee5d82e9df8cefd24139b9
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-01-12 16:48:41 +0500

    this keyword

--- objects/index.js
+++ objects/index.js
@@ -8,15 +8,80 @@
 //* =======================
 //? There are several ways to create objects in JavaScript. The most common one is using object literals:
 
+// const obj = {
+//     id: 1,          // id represent to key! aur iski value hai 1 
+//     pName: "Laptop",
+// };
+
+// let person = {
+//     name: "Hassan",
+//     age: 20,
+//     isStudent: false,
+//     greet: function (){
+//         console.log("Welcome to JS Course!");
+//     },
+// };
+
+// let person = {
+//     name: "Hassan",
+//     age: 20,
+//     "is'Student": false,
+//     greet: function (){
+//         console.log("Welcome to JS Course!");
+//     },
+// };
+
 //* =======================
 //* Accessing Properties:
 //* =======================
-//? You can access object propertiesusing dot notation or square bracket notation:
+//? You can access object properties using dot notation or square bracket notation:
+
+// let person = {
+//     name: "Hassan",
+//     age: 20,
+//     isStudent: false,
+//     greet: function (){
+//         console.log("Welcome to JS Course!");
+//     },
+// };
+
+// let person = {
+//     name: "Hassan",
+//     age: 20,
+//     "is'Student": false,
+//     greet: function (){
+//         console.log("Welcome to JS Course!");
+//     },
+// };
+
+// console.log(person.name);    // dot notation
+// console.log(person.age);
+// console.log(person.is'student);
+// console.log(person[`is'Student`]);   // square bracket notation
 
 //* ===================================
 //* Adding and Modifying Properties:
 //* ===================================
 //? You can add new properties or modify existing ones:
+
+// let person = {
+//     name: "Hassan",
+//     age: 20,
+//     "is'Student": false,
+//     greet: function (){
+//         console.log("Welcome to JS Course!");
+//     },
+// };
+
+// person.job = "Mern Stack developer";
+// // person.age = 14;
+// person["age"] = 15
+// console.log(person);
+
+
+// =====================
+// Methods:
+// =====================
 //? Methods in Objects are function associated with the object. They can be invoked using the same notation as properties:
 
 // console.log(person.greet());
@@ -77,3 +142,65 @@
 
 //  To avoid this behavior and create a true copy of the object, you can use methods like Object.assign() or the spread operator ( ... );
 
+//? Object.assign() is used to copy properties from one or more source objects to a target object.
+
+// ============================
+// Comparison by Reference:
+// ============================
+
+//? Two objects are equal only if they refer to the same object.
+//? Independent objects (even if they look alike) are not equal:
+
+// const obj1 = {name: "hassan"};
+// const obj2 = {name: "hassan"};
+// const obj3 = obj1;
+
+// const isEqual = obj1 === obj2 ? true: false
+// const isEqual = obj1 === obj3 ? true: false
+console.log(isEqual);
+
+// =====================================
+// JSON (JavaScript Object Notation):
+// =====================================
+
+
+//? JSON is a data interchange format derived from JavaScript objects. Objects can be easily converted to JSON and vice versa.
+
+// let student = {
+//     id: 1,
+//     sName: "Hassan",
+//     sAge: 20,
+//     isStudent: false,
+//     greet: function (){
+//         console.log(`hey my id is ${student.id} and my name is ${student.sName}`);
+//     },
+// };
+
+// let jsonData = JSON.stringify(student);
+// console.log(jsonData);
+// let parsedObject = JSON.parse(jsonData);
+// console.log(parsedObject);
+
+
+// =======================
+// "this" Object
+// =======================
+
+//? In JavaScript, the this keyword refers to an object.
+
+// which object depends on how this is being invoked (used or called).
+
+// The this keyword refers to different objects depending on how it is used:
+
+// In an object method, this refers to the object.
+// Alone, this refers to the global object.
+// In a function, this refers to the global object.
+// In a function, in strict mode, this is undefined.
+// In an event, this refers to the element that received the event.
+// Methods like call(), apply(), and bind() can refer this to any object.
+
+// ==========================
+//  NOTE :
+// ==========================
+
+// This is not a variable. It is a keyword, You cannot change the value of this.    
