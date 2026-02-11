bf214cdc1e1e922a72f65d5549442bfe0f8330d6
muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2026-02-11 12:50:24 +0500

    Objects end start js-works folder

--- objects/index.js
+++ objects/index.js
@@ -302,9 +302,19 @@ const product = {
 
 //? 1: Object.keys(): Return an array containing the names of all enumerable own properties of an object.
 
+// let getKeys = Object.keys(product);
+// console.log(getKeys);
+
+
 //? 2: Object.values(): Returns an array containing the values of  all enumerable own properties of an object.
 
+// let getValues = Object.values(product);
+// console.log(getValues);
+
 //? 3: Object.entries(): Returns an array containing arrays of key-values pairs for each enumerable own properties of an object.
+
+// let entries = Object.entries(product);
+// console.log(entries);
 
 //? 4: Object.hasOwnProperty(): Returns a boolean indicating whether the object has the specified property as an own property.
 // console.log(product.hasOwnProperty("name"));   //output: true
@@ -312,4 +322,61 @@ const product = {
 
 //? 5: Object.assign(): Copies the values of all enumerable own properties from one or more source objects to a target object.
 // const target = {a: 1, b: 2};
-// const target = {a: 1, b: 2};
+// const source = {b: 3, c: 4};
+// const mergedObject = Object.assign( target, source);
+// console.log(mergedObject);   //* output:  {a: 1 ,b: 3, c: 4} 
+
+//? 5: Object.freeze(): Freezes an object, preventing new properties from being added to it and existing properties from being modified or deleted.
+
+// Object.freeze(product);
+// product.id = "7867";
+// console.log(product);
+
+// ===================================
+// Interview Question - Objects
+// ===================================
+
+//! What will be the output?
+
+// const target = {a: 1, b: 10};
+// const source = {b: 5, c: 4};
+
+// const mergedObject = Object.assign({}, target, source);
+// console.log(mergedObject);
+
+// ===========================================
+// Interview Question - Object Manipulation
+// ===========================================
+//! Give an object representing a student, write a function to add a new subject with its corresponding grade to the student's record. Also check if the grades property its present or not?
+
+let student = {
+        name: "Pop",
+        age: 18,
+        grades: {
+                math: 90,
+                urdu: 80,
+                science: 75,
+        },
+};
+
+addSubjectGrade(student, "computer", 95);
+console.log(student);
+
+
+// ===========================================
+// Interview Question - Object Comparison
+// ===========================================
+
+//! Write a function that compares two objects to determine if they have the same properties and values.
+
+// Example usage:
+let objA = {name: "Alice", age: 20, city: "New York"};
+let objB = {name: "Alice", age: 30, city: "New York"};
+let objC = {name: "Pop", age: 25, city: "Florida"};
+
+const areObjectEqual = (obj1, obj2) => {
+ 
+}
+
+console.log(areObjectEqual(objA, objB));   // should return: true
+console.log(areObjectEqual(objA, objC));   // should return: false
